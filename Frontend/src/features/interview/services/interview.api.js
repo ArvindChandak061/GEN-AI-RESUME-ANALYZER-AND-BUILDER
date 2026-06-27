6b12e71b3be192d4b25ac16106c210cf6232d6a8
ArvindChandak061 <maasaraswati654@gmail.com>
2026-06-27 14:52:40 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-resume-analyzer-and-builder-o4x5.onrender.com",
     withCredentials: true,
 })
 
