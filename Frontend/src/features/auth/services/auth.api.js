bf918713c726d00b21894b9f5773a29a7b17aaed
ArvindChandak061 <maasaraswati654@gmail.com>
2026-06-27 14:48:23 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-resume-analyzer-and-builder-o4x5.onrender.com",
     withCredentials: true // to get access of cookies as axios by default dont allow it.
 })
 
